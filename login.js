4ba04f0a2188cc5aae5e612f1d260677b3ccbfad
2ll dev <99831559+2lldeev@users.noreply.github.com>
2023-03-24 13:25:07 +0000

    wew

--- login.js
+++ login.js
@@ -1,17 +1,18 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCININKa8pPVcVEzebYw5EEPJ4GZ4UhCgs",
-  authDomain: "dev-deploy-c3707.firebaseapp.com",
-  databaseURL: "https://dev-deploy-c3707-default-rtdb.firebaseio.com",
-  projectId: "dev-deploy-c3707",
-  storageBucket: "dev-deploy-c3707.appspot.com",
-  messagingSenderId: "103302520342",
-  appId: "1:103302520342:web:f7833651ac5a48d34a9f37",
-  measurementId: "G-048J8S9WWB"
-};
+    apiKey: "AIzaSyCININKa8pPVcVEzebYw5EEPJ4GZ4UhCgs",
+    authDomain: "dev-deploy-c3707.firebaseapp.com",
+    databaseURL: "https://dev-deploy-c3707-default-rtdb.firebaseio.com",
+    projectId: "dev-deploy-c3707",
+    storageBucket: "dev-deploy-c3707.appspot.com",
+    messagingSenderId: "103302520342",
+    appId: "1:103302520342:web:f7833651ac5a48d34a9f37",
+    measurementId: "G-048J8S9WWB"
+  };
   
   firebase.initializeApp(firebaseConfig);
   
   function login() {
+    console.log("wew")
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
   
